7b3f6e0b534998312a50721f4e492eb699d61a87
Niki4Dan <ydaniil256@yandex.ru>
2025-12-03 15:49:47 +0300

    Update form (NewIdeaPage)

--- backend/src/router/createIdea/index.ts
+++ backend/src/router/createIdea/index.ts
@@ -2,9 +2,10 @@ import { ideas } from '../../lib/ideas'
 import { trpc } from '../../lib/trpc'
 import { schema } from './input'
 
-
-
 export const createIdeaTrpcRoute = trpc.procedure.input(schema).mutation(({ input }) => {
+  if (ideas.find((idea) => idea.ideaNick === input.ideaNick)) {
+    throw Error('Idea with this nick already exists')
+  }
   ideas.unshift(input)
   return true
 })
--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,5 +1,6 @@
 import { schema } from '@project/backend/src/router/createIdea/input'
 import { useFormik } from 'formik'
+import { useState } from 'react'
 import { toFormikValidationSchema } from 'zod-formik-adapter'
 import { Input } from '../../components/Input'
 import { Segment } from '../../components/Segment'
@@ -7,7 +8,8 @@ import { Textarea } from '../../components/Textarea'
 import { trpc } from '../../lib/trpc'
 
 export const NewIdeaPage = () => {
-
+  const [successMessageVisible, setSuccessMessageVisible] = useState(false)
+  const [submittingError, setSubmittingError] = useState<string | null>(null)
   const createIdea = trpc.createIdea.useMutation()
   const formik = useFormik({
     initialValues: {
@@ -18,7 +20,19 @@ export const NewIdeaPage = () => {
     },
     validationSchema: toFormikValidationSchema(schema),
     onSubmit: async (values) => {
-      await createIdea.mutateAsync(values)
+      try {
+        await createIdea.mutateAsync(values)
+        formik.resetForm()
+        setSuccessMessageVisible(true)
+        setTimeout(() => {
+          setSuccessMessageVisible(false)
+        }, 3000)
+      } catch (error: any) {
+        setSubmittingError(error.message)
+        setTimeout(() => {
+          setSubmittingError(null)
+        }, 3000)
+      }
     },
   })
 
@@ -35,7 +49,11 @@ export const NewIdeaPage = () => {
         <Input name="description" label="Description" type="text" formik={formik} />
         <Textarea name="text" label="Text" formik={formik} />
         {!formik.isValid && !!formik.submitCount && <div style={{ color: 'red' }}>Some fields are invalid</div>}
-        <button type="submit">Create Idea</button>
+        {successMessageVisible && <div style={{ color: 'green' }}>Idea created</div>}
+        {!!submittingError && <div style={{ color: 'red' }}>{submittingError}</div>}
+        <button type="submit" disabled={formik.isSubmitting}>
+          {formik.isSubmitting ? 'Loading...' : 'Creat idea'}
+        </button>
       </form>
     </Segment>
   )
